81b0d5a80f396b627e7c48b688690deffe4f73c8
lightbringer1701 <lightbringer1701@gmail.com>
2026-09-14 07:48:18 +0300

    fix RegistryInterceptor

--- src/main/java/com/flexi/validationservice/config/RegistryInterceptor.java
+++ src/main/java/com/flexi/validationservice/config/RegistryInterceptor.java
@@ -12,11 +12,15 @@ public class RegistryInterceptor {
         return template -> {
             String traceId = MDC.get("traceId");
             if (traceId != null) {
-                template.header("X-traceId", traceId);
+                template.header("X-trace-id", traceId);
             }
             String schemaId = MDC.get("schemaId");
-            if (traceId != null) {
-                template.header("X-schemaId", schemaId);
+            if (schemaId != null) {
+                template.header("X-schema-id", schemaId);
+            }
+            String serviceName = "validation-service";
+            if (schemaId != null) {
+                template.header("X-service-name", serviceName);
             }
         };
     }
--- src/main/java/com/flexi/validationservice/service/ModelRegistryClient.java
+++ src/main/java/com/flexi/validationservice/service/ModelRegistryClient.java
@@ -1,11 +1,15 @@
 package com.flexi.validationservice.service;
 
+import com.flexi.validationservice.config.RegistryInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import tools.jackson.databind.JsonNode;
 
-@FeignClient(name = "model-registry")
+@FeignClient(
+        name = "model-registry",
+        configuration = RegistryInterceptor.class
+)
 public interface ModelRegistryClient {
     @GetMapping("api/v1/validations/{id}/versions/{version}")
     JsonNode getSchema(@PathVariable String id, @PathVariable int version);
